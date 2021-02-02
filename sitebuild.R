1c6b49396714660d23cc95e46be4a114580ad218
fawda123 <mbafs2012@gmail.com>
2021-02-02 09:32:45 -0500

    changed project link, fix to sitebuild so it doesn't delete itself

---
+++ sitebuild.R
@@ -0,0 +1,18 @@
+# clean
+torm <- list.files(pattern = '\\.R$', recursive = F)
+torm <- torm[!torm %in% c('index.R', 'sitebuild.R')]
+file.remove(torm)
+file.remove('data/data.zip')
+rmarkdown::clean_site()
+
+# build
+rmarkdown::render_site(encoding = 'UTF-8')
+tobld <- list.files(pattern = '\\.Rmd$', recursive = F)
+tobld <- tobld[!tobld %in% c('Data_and_Resources.Rmd', 'index.Rmd', 'setup.Rmd', 'setupcloud.Rmd')]
+sapply(tobld, knitr::purl, documentation = 0L)
+# source('R/dat_proc.R')
+
+# zip all r rscripts
+rfls <- list.files('.', pattern = '\\.R$')
+rfls <- rfls[!rfls %in% c('sitebuild.R', 'functions.R')]
+zip('scripts.zip', rfls)
