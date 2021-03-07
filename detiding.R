e66601851a70135e32463b1172624ce0274da1e6
fawda123 <mbafs2012@gmail.com>
2021-03-07 12:11:43 -0500

    detiding lesson draft done

--- detiding.R
+++ detiding.R
@@ -10,7 +10,7 @@ dim(apadbwq)
 range(apadbwq$datetimestamp)
 
 # keep only observations that passed qaqc chekcs
-apadbwq <- qaqc(apadbwq, qaqc_keep = '0')
+apadbwq <- qaqc(apadbwq, qaqc_keep = c('0', '1', '2', '3', '4', '5'))
 
 # check the results
 head(apadbwq)
@@ -19,7 +19,7 @@ range(apadbwq$datetimestamp)
 
 # import weather data, clean it up
 apaebmet <- import_local(path = 'data', station_code = 'apaebmet')
-apaebmet <- qaqc(apaebmet, qaqc_keep = '0')
+apaebmet <- qaqc(apaebmet, qaqc_keep = c('0', '1', '2', '3', '4', '5'))
 
 # check the results
 head(apaebmet)
@@ -27,7 +27,7 @@ dim(apaebmet)
 range(apaebmet$datetimestamp)
 
 # combine water quality and weather data
-apa <- comb(apadbwq, apaebmet, timestep = 15, method = 'union')
+apa <- comb(apadbwq, apaebmet, timestep = 60, method = 'union')
 
 # check the results
 head(apa)
@@ -80,3 +80,22 @@ apa <- na.omit(apa)
 
 # number of rows after
 nrow(apa)
+
+knitr::include_graphics('figure/tidecorex.png')
+
+# get time zone
+tz <- attr(apa$DateTimeStamp, which = 'tzone')
+lat <- 29.6747
+long <- 85.0583
+
+evalcor(apa, tz = tz, lat = lat, long = long)
+
+dtd <- wtreg(apa, tz = tz, lat = lat, long = long)
+
+head(dtd)
+
+library(plotly)
+
+plot_ly(dtd, x = ~DateTimeStamp, y = ~DO_obs, name = 'Observed DO', type = 'scatter', mode = 'lines') %>% 
+  add_trace(y = ~DO_nrm, name = 'Detided DO', mode = 'lines') %>% 
+  layout(xaxis = list(title = NULL), yaxis = list(title = "DO (mg/L)"))
